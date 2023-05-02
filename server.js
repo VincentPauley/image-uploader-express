4e396c82c99eeef6bb466a8545db231ff43604ff
Vinnie Pauley <vinniepauley@gmail.com>
2023-05-01 19:01:37 -0500

    upload feature POC working

--- server.js
+++ server.js
@@ -2,11 +2,25 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const app = express();
+const multer = require('multer');
 
-console.log();
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, __dirname + '/uploads/images');
+  },
+  filename: function(req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.jpg');
+  }
+});
+const upload = multer({ dest: __dirname + '/uploads/images', storage });
 
-app.get('/', (req, res) => {
-  res.send('something for fun');
+app.use(express.static('public'));
+
+app.post('/upload', upload.single('photo'), (req, res) => {
+  if (req.file) {
+    res.json(req.file);
+  } else throw 'error';
 });
 
 app.listen(process.env.PORT, () => {
